084a87abe7f7ea461f9884b729d32d01d1381ba4
albayck <35208924+albayck@users.noreply.github.com>
2019-01-22 04:28:42 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$sign = "c47dbde04e6f429c7af4193b4cb922a8d18dabd3";
+$aid = "cf0d133a6522dcea9cef98f0237906afd80ce1f7";
+$uuid = "cf0d133a6522dcea9cef98f0237906afd80ce1f7";
+$android_id = "66d3a80b14b50f96";
+?>
